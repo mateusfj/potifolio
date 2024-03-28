a9e5ec6b64e2319ed8869425d290bc1faae566f2
Mateus <93952397+mateusfj@users.noreply.github.com>
2024-03-28 13:01:53 -0300

    alteracoes

--- index.php
+++ index.php
@@ -8,8 +8,12 @@
     <!--FONTS GOOGLE-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!--FIM FONTS GOOGLE-->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+
     <link rel="stylesheet" href="assets/style.css?<?= rand() ?>">
     <title>Portifólio</title>
 </head>
@@ -23,7 +27,9 @@
         <div class="interface">
             <header class="cabecalho flex">
                 <div class="logo">
-                    <a href="index.html"><i class="bi bi-code-slash"></i> Mateus</a>
+                    <a href="index.html">
+                        <img src="assets/logo3.png" alt="">
+                    </a>
                 </div>
                 <nav class="menu-desktop">
                     <ul>
@@ -37,6 +43,31 @@
                     <a href="#"><button>CONTATO</button></a>
                 </div>
             </header>
+
+            <div class="txt-img">
+                <div class="img-topo">
+                    <img src="assets/img-topo-site.png" alt="">
+                </div>
+
+
+                <div class="txt-topo">
+                    <p>Olá, o meu nome é </p>
+                    <h1>MATEUS SOUSA</h1>
+                    <div class="maquina-escrever">
+                        <h2>Desenvolvedor Front-end</h2>
+                    </div>
+                    <div class="buttons">
+                        <div class="btn-cv">
+                            <a href=""><i class="bi bi-github"></i> Github</a>
+                        </div>
+                        <div class="btn-li">
+                            <a href=""><i class="bi bi-linkedin"></i> Likedin</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     </section>
 
