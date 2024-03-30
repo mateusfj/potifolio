554ba50dc4afa3887afcb574e59c536a7f03f3f7
Mateus <93952397+mateusfj@users.noreply.github.com>
2024-03-30 14:04:01 -0300

    footer concluido

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 
-    <link rel="stylesheet" href="assets/style.css?<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/style.css?<?= rand() ?>">
     <title>Portifólio</title>
 </head>
 
@@ -29,6 +29,10 @@
                     <a href="index.html">
                         <img src="assets/logo3.png" alt="">
                     </a>
+                </div>
+
+                <div class="menu-mobile">
+
                 </div>
                 <nav class="menu-desktop">
                     <ul>
@@ -60,7 +64,7 @@
                             <a href=""><i class="bi bi-github"></i> GitHub</a>
                         </div>
                         <div class="btn-li">
-                            <a href=""><i class="bi bi-github"></i> Linkedin</a>
+                            <a href=""><i class="bi bi-linkedin"></i> Linkedin</a>
                         </div>
                     </div>
                 </div>
@@ -70,7 +74,7 @@
         </div>
     </section>
 
-    <section class="interface">
+    <section class="interface size">
         <div class="header">
             <p class="txt_p">conheça mais</p>
             <h2 class="title">Sobre mim</h2>
@@ -129,40 +133,236 @@
             <p class="txt_p">Explorar meus</p>
             <h2 class="title">Projetos</h2>
         </div>
-        <div class="projetos">
+        <div class="projetos interface">
             <div class="conteiner">
+                <h3>Tela de Login - ADTR</h3>
+                <p>Tela de login que cadastras usuario e toda a parte de autenticação.</p>
+                <div class="icons-tech">
+                    <div class="icon">
+                        <img title="PHP" src="assets/php.png" alt="icone php">
+                    </div>
+                    <div class="icon">
+                        <img title="MYSQL" src="assets/mysql.png" alt="icone mysql">
+                    </div>
+                    <div class="icon">
+                        <img title="HTML" src="assets/html.png" alt="icone Html">
+                    </div>
+                    <div class="icon">
+                        <img title="CSS" src="assets/css.png" alt="icone css">
+                    </div>
+                </div>
                 <div class="contente-project">
                     <img src="assets/one-piece.jpeg" alt="">
-                    <h2>Site one piece</h2>
                 </div>
                 <div class="btn-project">
-                    <button>GitHub</button>
+                    <button>GitHub <i class="bi bi-github"></i></button>
                     <button>Ver Demo</button>
                 </div>
             </div>
             <div class="conteiner">
+                <h3>Tela de Login - ADTR</h3>
+                <p>Tela de login que cadastras usuario e toda a parte de autenticação.</p>
+                <div class="icons-tech">
+                    <div class="icon">
+                        <img src="assets/php.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/mysql.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/html.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/css.png" alt="">
+                    </div>
+                </div>
                 <div class="contente-project">
                     <img src="assets/one-piece.jpeg" alt="">
-                    <h2>Site one piece</h2>
                 </div>
                 <div class="btn-project">
-                    <button>GitHub</button>
+                    <button>GitHub <i class="bi bi-github"></i></button>
                     <button>Ver Demo</button>
                 </div>
             </div>
             <div class="conteiner">
+                <h3>Tela de Login - ADTR</h3>
+                <p>Tela de login que cadastras usuario e toda a parte de autenticação.</p>
+                <div class="icons-tech">
+                    <div class="icon">
+                        <img src="assets/php.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/mysql.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/html.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/css.png" alt="">
+                    </div>
+                </div>
                 <div class="contente-project">
                     <img src="assets/one-piece.jpeg" alt="">
-                    <h2>Site one piece</h2>
                 </div>
                 <div class="btn-project">
-                    <button>GitHub</button>
+                    <button>GitHub <i class="bi bi-github"></i></button>
                     <button>Ver Demo</button>
+                </div>
+            </div>
+            <div class="conteiner">
+                <h3>Tela de Login - ADTR</h3>
+                <p>Tela de login que cadastras usuario e toda a parte de autenticação.</p>
+                <div class="icons-tech">
+                    <div class="icon">
+                        <img src="assets/php.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/mysql.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/html.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/css.png" alt="">
+                    </div>
+                </div>
+                <div class="contente-project">
+                    <img src="assets/one-piece.jpeg" alt="">
+                </div>
+                <div class="btn-project">
+                    <button>GitHub <i class="bi bi-github"></i></button>
+                    <button>Ver Demo</button>
+                </div>
+            </div>
+            <div class="conteiner">
+                <h3>Tela de Login - ADTR</h3>
+                <p>Tela de login que cadastras usuario e toda a parte de autenticação.</p>
+                <div class="icons-tech">
+                    <div class="icon">
+                        <img src="assets/php.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/mysql.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/html.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/css.png" alt="">
+                    </div>
+                </div>
+                <div class="contente-project">
+                    <img src="assets/one-piece.jpeg" alt="">
+                </div>
+                <div class="btn-project">
+                    <button>GitHub <i class="bi bi-github"></i></button>
+                    <button>Ver Demo</button>
+                </div>
+            </div>
+            <div class="conteiner">
+                <h3>Tela de Login - ADTR</h3>
+                <p>Tela de login que cadastras usuario e toda a parte de autenticação.</p>
+                <div class="icons-tech">
+                    <div class="icon">
+                        <img src="assets/php.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/mysql.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/html.png" alt="">
+                    </div>
+                    <div class="icon">
+                        <img src="assets/css.png" alt="">
+                    </div>
+                </div>
+                <div class="contente-project">
+                    <img src="assets/one-piece.jpeg" alt="">
+                </div>
+                <div class="btn-project">
+                    <button>GitHub <i class="bi bi-github"></i></button>
+                    <button>Ver Demo</button>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section>
+        <div class="header">
+            <p class="txt_p">Entre em</p>
+            <h2 class="title">Contato</h2>
+        </div>
+        <div class="links">
+            <div class="links-ctt interface">
+                <div class="contato whatsapp">
+                    <i class="bi bi-whatsapp"></i>
+                    <div class="txt-contato">
+                        <p>86-98144-2421</p>
+                    </div>
+                </div>
+            </div>
+            <div class="links-ctt">
+                <div class="contato linkedin">
+                    <i class="bi bi-linkedin"></i>
+                    <div class="txt-contato">
+                        <p>Linked in</p>
+                    </div>
+                </div>
+            </div>
+            <div class="links-ctt">
+                <div class="contato email">
+                    <i class="bi bi-envelope"></i>
+                    <div class="txt-contato">
+                        <p>Mateusfj8144@gmail.com</p>
+                    </div>
                 </div>
             </div>
         </div>
+        <footer>
+            <div class="footer">
+                <div class="footer-nav">
+                    <h2>Navegue</h2>
+                    <ul>
+                        <li><a href="">Inicio</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Projetos</a></li>
+                        <li><a href="">Especialidades</a></li>
+                    </ul>
+                </div>
+                <div class="footer-nav">
+                    <h2>Projetos</h2>
+                    <ul>
+                        <li><a href="#"><i class="bi bi-window-fullscreen"></i> <span>Tela de login</span></a></li>
+                        <li><a href="#"><i class="bi bi-window-fullscreen"></i> <span>Tela de login</span></a></li>
+                        <li><a href="#"><i class="bi bi-window-fullscreen"></i> <span>Tela de login</span></a></li>
+                        <li><a href="#"><i class="bi bi-window-fullscreen"></i> <span>Tela de login</span></a></li>
+                    </ul>
+                </div>
+                <div class="footer-contato">
+                    <h2>Contato</h2>
+                    <div class="ctt-footer">
+                    <i class="bi bi-whatsapp"></i>
+                    <div class="txt-contato">
+                        <p>86-98144-2421</p>
+                    </div>
+                    </div>
+                    <div class="ctt-footer">
+                        <i class="bi bi-linkedin"></i>
+                        <div class="txt-contato">
+                            <p>Linked in</p>
+                        </div>
+                    </div>
+                    <div class="ctt-footer">
+                        <i class="bi bi-envelope"></i>
+                        <div class="txt-contato">
+                            <p>Mateusfj8144@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </section>
-    
+
     <script src="assets/script.js?<?= rand() ?>"></script>
 </body>
 
